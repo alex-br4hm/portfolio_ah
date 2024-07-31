e4bbead9fb4d21a0584d83dbffae27367ee6991e
br4hm <haehnlein.alexander@gmx.de>
2024-07-31 22:55:58 +0200

    implement/design about-me | small fixes (style)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeroComponent } from './hero/hero.component';
+
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
