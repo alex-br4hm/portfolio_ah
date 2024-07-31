541f3acf6986f7f0775a661627f0f18de9df044d
br4hm <haehnlein.alexander@gmx.de>
2024-07-31 14:15:39 +0200

    add/integrate font

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: HeroComponent }];
