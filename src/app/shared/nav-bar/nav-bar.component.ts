c9f05d8bbe4467d78b6bb0cef9bcc25bd0e59972
br4hm <haehnlein.alexander@gmx.de>
2024-08-05 18:11:51 +0200

    styled/implement nav full size / animations

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,12 +1,24 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
+  isOpened = false;
+  closingAnimation = false;
 
+  toggleNav() {
+    if (this.isOpened) {
+      this.closingAnimation = true;
+      setTimeout(() => {
+        this.isOpened = !this.isOpened;
+        this.closingAnimation = false;
+      }, 200);
+    } else this.isOpened = !this.isOpened;
+  }
 }
