298a987bec81d5a0a36b3af14278f30f36a40f79
br4hm <haehnlein.alexander@gmx.de>
2024-08-07 12:32:00 +0200

    refernce hover correct

--- src/app/references/references.component.ts
+++ src/app/references/references.component.ts
@@ -1,31 +1,43 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss',
 })
 export class ReferencesComponent {
   references = [
     {
+      isHovered: true,
       evaluator: 'Mailo Mittelstädt',
       evalatorPosition: 'Team Partner',
       reference:
         'Alex beeindruckt in Gruppenarbeiten durch sein umfangreiches Wissen und seine Expertise, die er stets bereitwillig mit dem Team teilt. Seine analytischen Fähigkeiten und präzisen Beiträge sorgen dafür, dass komplexe Probleme schnell und effektiv gelöst werden. Darüber hinaus ist Alex immer hilfsbereit und unterstützt seine Teammitglieder dabei, ihr Verständnis zu vertiefen und gemeinsam beste Ergebnisse zu erzielen.  ',
     },
     {
+      isHovered: false,
       evaluator: 'Joel Sygulla',
       evalatorPosition: 'Team Partner',
       reference:
         'Es war eine große Bereicherung, Alexander bei unserem Projekt Join als Projektpartner zu haben. Er konnte durch seine hervorragenden Fachkenntnisse viel zum Team beitragen und uns Kollegen gut unterstützen. Ich habe einiges von ihm gelernt und seine klaren und effektiven Kommunikationsfähigkeiten haben die Zusammenarbeit im Team erheblich verbessert. Es hat Spaß gemacht, mit ihm zusammenzuarbeiten, und ich kann ihn ohne Zweifel weiterempfehlen.',
     },
     {
+      isHovered: false,
       evaluator: 'Jonas Mahlburg',
       evalatorPosition: 'Team Partner',
       reference:
         'Mit Alexander in einem Team zusammen arbeiten zu dürfen war eine gute produktive Erfahrung. Alexander hat ein sehr gutes Wissen, über die Materie, konnte sich auch schnell in anderen Code einarbeiten und verstehen und ist ein prima Teamarbeiter. Über eine weitere Zusammenarbeit wäre ich sehr froh 🙂 ',
     },
   ];
+
+  index: number = 0;
+
+  onMouseEnter(index: number) {
+    this.references.forEach((reference, i) => {
+      reference.isHovered = i === index;
+    });
+  }
 }
